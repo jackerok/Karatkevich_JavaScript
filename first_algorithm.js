ac5fcdb7f412d3bb961fd462b5a03e809e447838
Evgeni Korotkevich <evgenikorotkevich25@gmail.com>
2025-12-12 12:41:09 +0400

    feat: complete first Algoritgm

---
+++ first_algorithm.js
@@ -0,0 +1,11 @@
+function numCompare(num) {
+  if (num > 7) {
+    return "Hello";
+  } else {
+    return "";
+  }
+}
+
+// !!! PRACTICE !!!
+console.log(numCompare(8));
+console.log(numCompare(5));
